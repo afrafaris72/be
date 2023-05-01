2e8e59387dcb19700aafd76eaca7758836bf0023
afrafaris72 <afra.faris123@gmail.com>
2023-05-01 17:49:58 +0700

    be

--- dto/auth/auth_request.go
+++ dto/auth/auth_request.go
@@ -2,12 +2,13 @@ package authdto
 
 type AuthRequest struct {
 	Id       int    `json:"id"`
-	Email    string `json:"email" form:"email" validate:"required"`
+	Email    string `json:"email" binding:"required, email" gorm:"unique, not null" `
 	Password string `json:"password" form:"password" validate:"required"`
 	FullName string `json:"fullName" form:"fullName" validate:"required"`
 	Gender   string `json:"gender" form:"gender" validate:"required"`
 	Phone    string `json:"phone" form:"phone" validate:"required"`
 	Address  string `json:"address" form:"address" validate:"required"`
+	Role     string `json:"role" form:"role"`
 }
 type LoginRequest struct {
 	Email    string `json:"email" form:"email"`
