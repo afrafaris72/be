9671d54e7a5dde30f69244cd82e8be93908b6a91
afrafaris72 <afra.faris123@gmail.com>
2023-05-01 17:35:56 +0700

    be

--- main.go
+++ main.go
@@ -5,31 +5,38 @@ import (
 	"dumbflix/pkg/mysql"
 	"dumbflix/routes"
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	e := echo.New()
+
 	errEnv := godotenv.Load()
 	if errEnv != nil {
 		panic("Failed to load env file")
 	}
-	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+	}))
+
 	mysql.DatabaseInit()
 	database.RunMigrations()
 
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},                                                 // mengijinkan akses semuanya
-		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},        // mengijinkan method apa aja yg bisa digunakan
-		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"}, // mengijinkan headers apa aja yg bisa digunakan
-	}))
-
-	routes.RouteInit(e.Group("/api/v1"))
+	// e.GET("/Login", Lgn)
+	// e.GET("/Passsword", Pw)
 
 	e.Static("/uploads", "./uploads")
+	routes.RouteInit(e.Group("/api/v1"))
+	var PORT = os.Getenv("PORT")
+	fmt.Println("Server berjalan di :" + PORT)
 
-	fmt.Println("Run at localhost:5000")
-	e.Logger.Fatal(e.Start("localhost:5000"))
+	e.Logger.Fatal(e.Start(":" + PORT)) // delete localhost
 }
